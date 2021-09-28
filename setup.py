543db789d78aa4cce0e99110c313ea6d12dc43b8
Enzyme125 <axoford@icloud.com>
2021-09-28 10:03:15 +0800

    基础功能

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='分析化学实验',
+    version='1.0.0',
+    packages=['anal_chem'],
+    url='',
+    license='MIT',
+    author='Enzyme125',
+    author_email='847072154@qq.com',
+    description='分析化学中常用函数实现'
+)
